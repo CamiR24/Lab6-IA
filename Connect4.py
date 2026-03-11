3197b9b427e5d6224cc0795ca19427a0aea846ae
Camila Richter <ric23183@uvg.edu.gt>
2026-03-11 16:57:16 -0600

    funciòn para definir si es válida la victoria tomando en cuenta vertical, horizontal y diagonales

--- Connect4.py
+++ Connect4.py
@@ -7,7 +7,7 @@ COLS = 7
 
 class Connect4:
     
-    def __init__(self):
+    def __init__(self): #inicializar
         self.board = np.zeros((ROWS, COLS), dtype=int)
     
     def copy(self):
@@ -24,7 +24,34 @@ class Connect4:
                 self.board[row][col] = player
                 break
     
-    def is_terminal(self):
+    def is_terminal(self): #termina el juego
         return self.check_winner(1) or self.check_winner(-1) or len(self.actions()) == 0
+    
+    def check_winner(self, player): #verifica la victoria 
+        # Horizontal
+        for r in range(ROWS):
+            for c in range(COLS - 3): #misma fila, solo cambia de columna
+                if all(self.board[r][c+i] == player for i in range(4)):
+                    return True
+        
+        # Vertical
+        for r in range(ROWS - 3):
+            for c in range(COLS): #misma columna, solo cambia de fila
+                if all(self.board[r+i][c] == player for i in range(4)):
+                    return True
+        
+        # Diagonal derecha
+        for r in range(ROWS - 3):
+            for c in range(COLS - 3): #cambia de columna de y de fila con pendiente positiva
+                if all(self.board[r+i][c+i] == player for i in range(4)):
+                    return True
+        
+        # Diagonal izquierda
+        for r in range(3, ROWS):
+            for c in range(COLS - 3): #cambia de columna de y de fila con pendiente negativa
+                if all(self.board[r-i][c+i] == player for i in range(4)):
+                    return True
+        
+        return False
     
     
